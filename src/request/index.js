7db8ea4b97e0ddadad5e1dfcc2403873db7ca2b8
WB20230720014 <wb20230720014@djbx.com>
2024-04-01 15:32:24 +0800

    创建 axios 拦截器

---
+++ src/request/index.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+let config = {
+    baseURL: "https://localhost:8081/api",
+    timeout: 5000
+}
+
+const instance = axios.create({...config});
+instance.interceptors.request.use(requestSuccessInterceptor, requestErrorInterceptor);
+instance.interceptors.response.use(responseSuccessInterceptor, responseErrorInterceptor);
+
+/**
+ * 请求拦截器，在发送请求前拦截
+ * @param config
+ * @returns {*}
+ */
+export function requestSuccessInterceptor(config) {
+    console.log("请求前成功")
+    return config;
+}
+
+/**
+ * 对请求错误做什么
+ * @param error
+ * @returns {Promise<never>}
+ */
+export function requestErrorInterceptor(error) {
+    console.log("请求前失败")
+    return Promise.reject(error);
+}
+
+/**
+ * 响应拦截器，对响应数据做点什么
+ * @param response
+ * @returns {*}
+ */
+export function responseSuccessInterceptor(response) {
+    console.log("请求前失败")
+    return response;
+}
+
+/**
+ * 对响应错误做什么
+ * @param error
+ * @returns {Promise<never>}
+ */
+export function responseErrorInterceptor(error) {
+    console.log("请求前失败")
+    return Promise.reject(error);
+}
+
+export default instance;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import instance from "@/request/index.js";
+instance({
+  method: "get",
+  url: "http://127.0.0.1:8081/api/v1/pub/video/list_banner",
+  data: {
+
+  }
+})
+
+
+
 </script>
 
 <template>
