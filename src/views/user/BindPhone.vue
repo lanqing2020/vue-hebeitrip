b0f081cbe7b530f3b6b953e0c5f81fea208070c1
Chris <313680654@qq.com>
2024-10-08 17:56:17 +0800

    btn按钮

--- src/views/user/BindPhone.vue
+++ src/views/user/BindPhone.vue
@@ -4,6 +4,11 @@ import { ref } from "vue";
 
 const phone = ref(null);
 const smsCode = ref(null);
+const btnStatus = ref(false);
+
+const onClickBindBtn = () => {
+  // 保存信息接口
+}
 </script>
 
 <template>
@@ -24,8 +29,26 @@ const smsCode = ref(null);
       </template>
     </van-field>
   </van-cell-group>
+  <div class="btn-wrap">
+    <van-button :disabled="!btnStatus" round type="success" class="save-button" @click="onClickBindBtn">完成绑定</van-button>
+  </div>
 </template>
 
 <style scoped lang="less">
-
+.btn-wrap {
+  width: 100%;
+  margin-top: 50px;
+  padding: 0 35px;
+  box-sizing: border-box;
+  .save-button {
+    width: 100%;
+    background: linear-gradient(90deg, #fe7b21, #fe5010);
+    border: none;
+    /deep/ div {
+      .van-button__text {
+        font-size: 32px;
+      }
+    }
+  }
+}
 </style>
