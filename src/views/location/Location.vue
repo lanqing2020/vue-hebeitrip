a387efcab03793867f3ca600a71a38d4de8b9591
Chris <313680654@qq.com>
2024-10-14 22:13:39 +0800

    修改city为location

--- src/views/location/Location.vue
+++ src/views/location/Location.vue
@@ -3,7 +3,7 @@ import router from "@/router/index.js";
 import SearchComp from "@/components/search/Search.vue";
 import {computed, onMounted, reactive, ref} from "vue";
 
-const hotCities = reactive([
+const hotLocations = reactive([
   { id: 0, title: "华清宫", src: "https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" },
   { id: 1, title: "陕西历史博物馆", src: "https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" },
   { id: 2, title: "华山", src: "https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" },
@@ -14,42 +14,31 @@ const hotCities = reactive([
 const numbersOfItems = ref(4);
 
 // 计算属性 缓存数组数据
-const hotCitiesComputed = computed(() => {
-  return hotCities.slice(0, numbersOfItems.value);
+const hotLocationsComputed = computed(() => {
+  return hotLocations.slice(0, numbersOfItems.value);
 })
-const hotCityLen = ref(0);
+
 // 通过切换要循环的箱数控制前台的更多或折叠
-const switchCities = () => {
-  numbersOfItems.value = numbersOfItems.value === 4 ? hotCities.length : 4;
-  adjustNumber()
-}
-const adjustNumber = () => {
-  let len = Math.floor(hotCitiesComputed.value.length / 2);
-  if(hotCities.length % 2 !==0){
-    len+=1;
-  }
-  hotCityLen.value = len;
-}
-const moreCity = (val) => {
-  router.push({path: `/city/more`,query: {id: val}});
+const switchLocations = () => {
+  numbersOfItems.value = numbersOfItems.value === 4 ? hotLocations.length : 4;
 }
 
-onMounted(()=>{
-  adjustNumber()
-})
+const moreLocation = (val) => {
+  router.push({path: `/city/more`,query: {id: val}});
+}
 
 </script>
 
 <template>
   <SearchComp />
   <main>
-    <div class="hotCity" :style="{height:hotCityLen*3.67+1+'rem'}">
+    <div class="hotLocation">
       <div class="title">
         <span class="hot_name">热门目的地</span>
-        <span class="more" @click="switchCities">{{ numbersOfItems === 4 ? "更多" : "收起" }}</span>
+        <span class="more" @click="switchLocations">{{ numbersOfItems === 4 ? "更多" : "收起" }}</span>
       </div>
-      <div class="hotCity_list" :style="{height:hotCityLen*3.67}">
-        <div class="hotCity_list_item" v-for="(item, index) in hotCities" :key="index" @click="router.push(`/city/detail?id=${ item.id }`)">
+      <div class="hotLocation_list">
+        <div class="hotLocation_list_item" v-for="(item, index) in hotLocationsComputed" :key="index" @click="router.push(`/location/detail?id=${ item.id }`)">
           <van-image height="100%" :src="item.src">
             <template v-slot:loading>
               <van-loading type="spinner" size="20" />
@@ -59,13 +48,13 @@ onMounted(()=>{
         </div>
       </div>
     </div>
-    <div class="hotCity-text">
+    <div class="hotLocation-text">
       <div class="title">
         <span class="hot_name">河北</span>
-        <span class="more" @click="moreCity">探索</span>
+        <span class="more" @click="moreLocation">探索</span>
       </div>
       <div class="text-span">
-        <span v-for="(item, index) in hotCities" :key="index" @click="router.push(`/city/detail?id=${ item.id }`)">{{ item.title }}</span>
+        <span v-for="(item, index) in hotLocations" :key="index" @click="router.push(`/location/detail?id=${ item.id }`)">{{ item.title }}</span>
       </div>
     </div>
   </main>
@@ -77,14 +66,12 @@ main {
   padding: 0 30px 140px 30px;
   box-sizing: border-box;
   border-top: 1px solid #ebedf0;
-  .hotCity{
-    height: 0;
+  .hotLocation{
     transition: all .3s ease-in-out;
     overflow: hidden;
     &_list{
       display: flex;
       flex-wrap: wrap;
-      height: 0;
       transition: all .3s ease-in-out;
       justify-content: space-between;
       &_item{
@@ -110,7 +97,7 @@ main {
       }
     }
   }
-  .hotCity-text {
+  .hotLocation-text {
     margin-top: 50px;
     .text-span {
       span {
